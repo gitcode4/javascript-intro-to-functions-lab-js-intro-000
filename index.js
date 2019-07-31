db2ab8314b940cb54d4e37fb9a950d858888cac8
Liz <lizphil88@gmail.com>
2019-07-31 00:36:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-function sayHiToGrandma(string)
-{
-var uppercase = "HELLO!";
-  uppercase.toUpperCase() === uppercase; // true
-  if (uppercase){
-  return "YES INDEED!";
-  }
-}
-
-function sayHiToGrandma(string)
-{
-var greeting = "I love you, Grandma";
-  if (greeting){
-  return "I love you, too.";
-  }
+function shout(string) {
+  return string.toUpperCase()
 }
