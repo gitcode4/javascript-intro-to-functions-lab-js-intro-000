8b2c8c076ddb901bd73c5594d66e9349a192bb3b
Liz <lizphil88@gmail.com>
2019-07-31 01:14:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,17 +25,19 @@ var lowercase = "hello!";
 
 function sayHiToGrandma(string)
 {
-var greeting = "I love you, Grandma";
-  if (greeting){
-  return "I love you, too.";
-  }
-}
-
-function sayHiToGrandma(string)
-{
 var uppercase = "HELLO!";
   uppercase.toUpperCase() === uppercase; // true
   if (uppercase){
   return "YES INDEED!";
   }
 }
+
+function sayHiToGrandma(string)
+{
+var greeting = "I love you, Grandma";
+  if (greeting){
+  return "I love you, too.";
+  }
+}
+
+
